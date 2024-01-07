3bb2c0cfbcc9d178c291d288caaa11903ff234b7
masajinobe-ef <priscilla.effects@gmail.com>
2024-01-07 21:24:06 +0500

    minor

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,34 +1,15 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
+# scoop-search
 Invoke-Expression (&scoop-search --hook)
 
-oh-my-posh --init --shell pwsh --config 'C:\Users\masa\Documents\PowerShell\owl.json' | Invoke-Expression
+# oh-my-posh & posh-git
+oh-my-posh --init --shell pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/amro.omp.json' | Invoke-Expression
 Import-Module posh-git
 
+# Icons
 Import-Module -Name Terminal-Icons
-
-# PSReadLine
-Import-Module PSReadLine
-# Use inline prediction view in VSCode to avoid: WARNING: The prediction 'ListView' is temporarily disabled because the current window size of the console is too small. To use the 'ListView', please make sure the 'WindowWidth' is not less than '54' and the 'WindowHeight' is not less than '15'.
-if($env:TERM_PROGRAM -eq "vscode") {
-   Set-PSReadLineOption -PredictionViewStyle InlineView
-}else {
-   Set-PSReadLineOption -PredictionViewStyle ListView
-}
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -ShowToolTips
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+f'
 
 # Alias
 Set-Alias c code
@@ -38,12 +19,39 @@ Set-Alias scoof scoop
 Set-Alias g git
 Set-Alias l ls
 Set-Alias grep findstr
-Set-Alias lg lazygit 
+Set-Alias lg lazygit
 
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+function sy {
+    scoop update
+    scoop status
 }
+
+function syu {
+    scoop update
+    scoop status
+    scoop update *
+}
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+# Use inline prediction view in VSCode to avoid: WARNING: The prediction 'ListView' is temporarily disabled because the current window size of the console is too small. To use the 'ListView', please make sure the 'WindowWidth' is not less than '54' and the 'WindowHeight' is not less than '15'.
+if($env:TERM_PROGRAM -eq "vscode") {
+    Set-PSReadLineOption -PredictionViewStyle InlineView
+ }else {
+    Set-PSReadLineOption -PredictionViewStyle ListView
+ }
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+f'
 
 cls
