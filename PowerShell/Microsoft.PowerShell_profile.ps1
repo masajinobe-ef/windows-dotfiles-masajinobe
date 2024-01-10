d789dbce7d86f93b59fa9c72510ae67b56bde4e3
masajinobe-ef <priscilla.effects@gmail.com>
2024-01-10 09:01:27 +0500

    fix

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -11,51 +11,57 @@ Import-Module posh-git
 # Icons
 Import-Module -Name Terminal-Icons
 
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+t' 
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f'
+
+# Completion
+Import-Module -Name CompletionPredictor
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -BellStyle Visual
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle InlineView
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -Colors @{ InlinePrediction = "$([char]0x1b)[36;7;238m"}
+
 # Alias
 Set-Alias c code
 Set-Alias n notepad
 Set-Alias s scoop
-Set-Alias scoof scoop
 Set-Alias g git
 Set-Alias l ls
 Set-Alias grep findstr
 Set-Alias lg lazygit
 
 function sy {
-    scoop update
-    scoop status
+    s update
+    s status
 }
 
 function syu {
-    scoop update
-    scoop status
-    scoop update *
+    s update
+    s status
+    s update *
+}
+
+function ss {
+    $query = scoop-search | fzf --query="$args"
+    scoop-search $query
 }
 
 function live {
-    live-server --port=5500 --no-browser
+    live-server --port=5500
 }
 
-# PSReadLine
-Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -ShowToolTips
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 # Use inline prediction view in VSCode to avoid: WARNING: The prediction 'ListView' is temporarily disabled because the current window size of the console is too small. To use the 'ListView', please make sure the 'WindowWidth' is not less than '54' and the 'WindowHeight' is not less than '15'.
 if($env:TERM_PROGRAM -eq "vscode") {
     Set-PSReadLineOption -PredictionViewStyle InlineView
- }else {
-    Set-PSReadLineOption -PredictionViewStyle ListView
  }
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+f'
-
-cls
