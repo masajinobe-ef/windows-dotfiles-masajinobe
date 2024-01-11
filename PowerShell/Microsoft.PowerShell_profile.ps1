6d4b03d7438aabc6f64d48ba5fc4c6c5b8030841
masajinobe-ef <priscilla.effects@gmail.com>
2024-01-11 07:13:59 +0500

    fix

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,32 +5,36 @@
 Invoke-Expression (&scoop-search --hook)
 
 # oh-my-posh & posh-git
-oh-my-posh --init --shell pwsh --config 'C:\Users\masa\Documents\PowerShell\bubbles.omp.json' | Invoke-Expression
+$profileDirectory = (Get-Item $PROFILE).Directory.FullName
+$relativeConfigPath = Join-Path $profileDirectory "jandedobbeleer.omp.json"
+oh-my-posh --init --shell pwsh --config $relativeConfigPath | Invoke-Expression
 Import-Module posh-git
 
 # Icons
 Import-Module -Name Terminal-Icons
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+t' 
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f'
 
 # Completion
 Import-Module -Name CompletionPredictor
 
 # PSReadLine
 Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -ShowToolTips
 Set-PSReadLineOption -BellStyle Visual
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle InlineView
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -key Enter -Function ValidateAndAcceptLine
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -Colors @{ InlinePrediction = "$([char]0x1b)[36;7;238m"}
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+t' 
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f'
 
 # Alias
 Set-Alias c code
@@ -41,27 +45,33 @@ Set-Alias l ls
 Set-Alias grep findstr
 Set-Alias lg lazygit
 
+# Use inline prediction view in VSCode to avoid: 
+# WARNING: The prediction 'ListView' is temporarily disabled because the current window size of the console is too small. 
+# To use the 'ListView', please make sure the 'WindowWidth' is not less than '54' and the 'WindowHeight' is not less than '15'.
+if($env:TERM_PROGRAM -eq "vscode") {
+    Set-PSReadLineOption -PredictionViewStyle InlineView
+ }
+
+# scoop update
 function sy {
     s update
     s status
 }
 
+# scoop update *
+function su {
+    s update *
+    s status
+}
+
+# scoop sync packages and update
 function syu {
     s update
     s status
     s update *
 }
 
-function ss {
-    $query = scoop-search | fzf --query="$args"
-    scoop-search $query
-}
-
+# live-server
 function live {
     live-server --port=5500
 }
-
-# Use inline prediction view in VSCode to avoid: WARNING: The prediction 'ListView' is temporarily disabled because the current window size of the console is too small. To use the 'ListView', please make sure the 'WindowWidth' is not less than '54' and the 'WindowHeight' is not less than '15'.
-if($env:TERM_PROGRAM -eq "vscode") {
-    Set-PSReadLineOption -PredictionViewStyle InlineView
- }
